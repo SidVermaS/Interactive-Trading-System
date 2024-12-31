c745eacb17fb2241ff91d71e84717fc13b4212d5
Sid <sidvermas1234@gmail.com>
2024-12-31 18:00:51 +0530

    createOrders simulated array

--- backend/src/modules/simulate/simulate.ts
+++ backend/src/modules/simulate/simulate.ts
@@ -1,7 +1,8 @@
-import { TradingPair } from "@prisma/client"
+import { Prisma, TradingPair } from "@prisma/client"
 import { TradingPairModule } from "../currency/tradingPair"
 import { simulatePriceChange } from "../../utils/simulate/simulate"
 import ClientModule from "../user/client"
+import { randomFloat, randomInt } from "../../utils/common/data"
 
 const SimulateModule = {
   priceChange: async (tradingPair: TradingPair) => await TradingPairModule.update({
@@ -14,13 +15,34 @@ const SimulateModule = {
     await Promise.allSettled(tradingPairResult.map((tradingPair) => SimulateModule.priceChange(tradingPair)))
   },
   createOrders: async (noOfOrders: number = 10) => {
-    const clientsResult = await ClientModule.fetch()
+    const [clientsResult, tradingPairResult] = await Promise.all([ClientModule.fetch(undefined, undefined, undefined, { id: true }), TradingPairModule.fetchById({ symbol: 'BTCUSDT' })])
+
+    const tradingPairId = tradingPairResult.id
     const clientsCount = clientsResult.length;
-
-    const tradingPairResult = await TradingPairModule.fetchById({ symbol: 'BTCUSDT' })
-
+    const simulatedOrders: Prisma.OrderUncheckedCreateInput[] = []
     for (let index = 0; index < Math.floor(noOfOrders / 2); index++) {
-
+      const buyClientId = clientsResult[randomInt(0, clientsCount)].id
+      const sellClientId = clientsResult[randomInt(0, clientsCount)].id
+      // 5 minutes, 2 hours
+      const expiration = new Date(new Date().getTime() + (randomInt(300000, 7200000)))
+      const price = randomFloat(tradingPairResult.minPrice.toNumber(), tradingPairResult.maxPrice.toNumber())
+      const quantity = randomFloat(tradingPairResult.minQty.toNumber(), tradingPairResult.maxQty.toNumber())
+      const buyOrder: Prisma.OrderUncheckedCreateInput = {
+        clientId: buyClientId,
+        expiration,
+        type: 'BUY',
+        price,
+        quantity,
+        tradingPairId
+      }
+      const sellOrder: Prisma.OrderUncheckedCreateInput = {
+        clientId: sellClientId,
+        expiration,
+        type: 'SELL',
+        price,
+        quantity, tradingPairId
+      }
+      simulatedOrders.push(buyOrder, sellOrder)
     }
 
   }
--- backend/src/utils/common/data.ts
+++ backend/src/utils/common/data.ts
@@ -3,3 +3,8 @@ export const randomFloat = (min: number, max: number, decimals: number = 2) => {
   const factor = Math.pow(10, decimals)
   return Math.round((Math.random() * (max - min) + min) * factor) / factor;
 }
+export const randomInt = (min: number, max: number) => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min+1)) + min
+}
